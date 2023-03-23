830d9dbac831d7e5cf7df24cd7dfdf72c78c9f51
zhouzhiyang <zhouzhiyang@xiaobing.ai>
2023-03-23 11:59:05 +0800

    Upload sumary page

---
+++ sumary.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+
+## Sidebar
+sidebar = st.sidebar
+
+with sidebar:
+    st.subheader("About It")
+    st.info("This is a sumary intended to list some display demos about AI speech, "
+            "which is hosted with [Streamlit](https://streamlit.io/cloud).")
+
+    st.subheader("Contact")
+    st.markdown("Maintained by Zhou (zhouzhiyang@xiaobing.ai)")
+
+
+## Main Area
+st.title("__Sumary__")
+# st.markdown("""
+# > Author: Atom (atomicoo95@gmail.com)  
+# > For more details, please see [source code](https://github.com/atomicoo/IceDemo).  
+# """)
+# st.markdown("")
+
+st.subheader("TTS Demo")
+st.markdown("""
+- Ice TTS vits: https://ice-tts-vits.streamlit.app/
+""")
+
+
+st.subheader("SRC Demo")
+st.markdown("""
+- Ice SRC resnet: https://ice-src-resnet.streamlit.app/
+""")
+
