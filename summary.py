011a814af11fd68a30d42d4f9e3fa7bbbea6cdea
zhouzhiyang <zhouzhiyang@xiaobing.ai>
2023-03-24 11:39:37 +0800

    Upload README

--- summary.py
+++ summary.py
@@ -6,8 +6,9 @@ sidebar = st.sidebar
 
 with sidebar:
     st.subheader("About It")
-    st.info("This is a sumary intended to list some display demos about AI speech, "
-            "which is hosted with [Streamlit](https://streamlit.io/cloud).")
+    st.info("This is a summary intended to list some display demos about AI speech, "
+            "which is hosted with [Streamlit](https://streamlit.io/cloud). "
+            "For more details, please see the [Github repository](https://github.com/atomicoo/IceDemo).")
 
     st.subheader("Contact")
     st.markdown("Maintained by Zhou (zhouzhiyang@xiaobing.ai)")
