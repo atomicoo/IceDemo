3b0c4f687c213b87b77f01d9ace321b1c231e958
Zhiyang Zhou <zhouzhiyang@xiaobing.ai>
2023-03-27 23:02:32 +0800

    Upload summary page

---
+++ summary.py
@@ -0,0 +1,36 @@
+import streamlit as st
+
+
+## Sidebar
+sidebar = st.sidebar
+
+with sidebar:
+    st.subheader("About It")
+    st.info("This is a summary intended to list some display demos about AI speech, "
+            "which is hosted with [Streamlit](https://streamlit.io/cloud). "
+            "For more details, please see the [Github repository](https://github.com/atomicoo/IceDemo).")
+
+    st.subheader("Contact")
+    st.markdown("Maintained by Zhou (zhouzhiyang@xiaobing.ai)")
+
+
+## Main Area
+st.title("__Summary__")
+# st.markdown("""
+# > Author: Atom (atomicoo95@gmail.com)  
+# > For more details, please see [source code](https://github.com/atomicoo/IceDemo).  
+# """)
+# st.markdown("")
+
+st.subheader("TTS Demo")
+st.markdown("""
+- Ice TTS vits: https://ice-tts-vits.streamlit.app/
+- Ice TTS style: https://ice-tts-style.streamlit.app/
+""")
+
+
+st.subheader("SRC Demo")
+st.markdown("""
+- Ice SRC resnet: https://ice-src-resnet.streamlit.app/
+""")
+
